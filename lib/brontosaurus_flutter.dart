543e0c65a864ac27128ce17113287375b683dfe1
WMXPY <wm@mengw.io>
2019-12-17 12:26:08 -0600

    init base

--- lib/brontosaurus_flutter.dart
+++ lib/brontosaurus_flutter.dart
@@ -1,7 +1,39 @@
 library brontosaurus_flutter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+class BrontosaurusView extends StatelessWidget {
+  final String server;
+  final String application;
+
+  BrontosaurusView({
+    @required this.server,
+    @required this.application,
+  }) {
+    _init();
+  }
+
+  final webview = FlutterWebviewPlugin();
+
+  void _init() {
+    webview.onUrlChanged.listen((state) {
+      final bool redirected = state.indexOf(server) == -1;
+      if (redirected) {
+        print(state);
+      }
+    });
+  }
+
+  String _buildUrl() {
+    return server + '?key=' + application + '&cb=js://redirect';
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebviewScaffold(
+      url: _buildUrl(),
+      resizeToAvoidBottomInset: true,
+    );
+  }
 }
